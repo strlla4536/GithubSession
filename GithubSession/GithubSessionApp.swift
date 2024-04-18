542bcedc5ef21aa07fd3c7fe59d17da9138e6a7a
strlla4536 <strlla4536@gmail.com>
2024-04-18 20:19:45 +0900

    Initial Commit

---
+++ GithubSession/GithubSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSessionApp.swift
+//  GithubSession
+//
+//  Created by 규북 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
